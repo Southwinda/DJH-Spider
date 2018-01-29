a35e90e1b03662617ce9a6d3abf8a610e9deaf60
jasonhavenD <jasonhaven@163.com>
2018-01-29 23:25:24 +0800

    update douyu

--- Scrapy/day5/DouYu/DouYu/items.py
+++ Scrapy/day5/DouYu/DouYu/items.py
@@ -11,4 +11,5 @@ import scrapy
 class DouyuItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    nickname=scrapy.Field()
+    vertical_src=scrapy.Field()
---
+++ Scrapy/day5/DouYu/DouYu/spiders/douyu.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+# from Douyu.items import DouyuItem
+
+
+class DouyuSpider(scrapy.Spider):
+    name = 'douyu'
+    allowed_domains = ['douyucdn.cn']
+    start_urls = [
+        'http://capi.douyucdn.cn/api/v1/getVerticalRoom?limit=20&offset=']
+
+    def parse(self, response):
+        datas = json.loads(response.body)
+        for data in datas['data']:
+            # item = DouyuItem()
+            # item['vertical_src'] = data['vertical_src']
+            # item['nickname'] = data['nickname']
+            # yield item
+            vertical_src = data['vertical_src']
+            nickname = data['nickname']
+            print(vertical_src,nickname)
