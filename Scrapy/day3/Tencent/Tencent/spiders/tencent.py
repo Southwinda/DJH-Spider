db88794560f236db4c9160789fdc2eeceabca9f3
JasonhavenDai <951771966@qq.com>
2018-01-29 21:06:34 +0800

    update day3 tencent

--- Scrapy/day3/Tencent/Tencent/spiders/tencent.py
+++ Scrapy/day3/Tencent/Tencent/spiders/tencent.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
-
 class TencentSpider(scrapy.Spider):
     name = 'tencent'
-    allowed_domains = ['tencent.com']
-    start_urls = ['http://tencent.com/']
+    allowed_domains = ['hr.tencent.com']
+    start_urls = ['https://hr.tencent.com/position.php?lid=2156&tid=87&keywords=python']
 
     def parse(self, response):
         pass 
